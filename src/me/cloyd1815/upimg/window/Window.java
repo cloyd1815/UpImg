e25c0a71c0b30dd5207f4b2bce27fef502c131ca
Colin <loyd3152@gmail.com>
2014-03-10 10:57:02 -0400

    hp

--- src/me/cloyd1815/upimg/window/Window.java
+++ src/me/cloyd1815/upimg/window/Window.java
@@ -1,24 +1,35 @@
 package me.cloyd1815.upimg.window;
 
 import java.awt.AWTException;
+import java.awt.Image;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
 import java.awt.Rectangle;
 import java.awt.Robot;
+import java.awt.SystemTray;
 import java.awt.Toolkit;
+import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowStateListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.UIManager;
 
 import me.cloyd1815.upimg.UpImg;
+import me.cloyd1815.upimg.main.Main;
 
 public class Window extends JFrame implements ActionListener {
 
@@ -28,25 +39,87 @@ public class Window extends JFrame implements ActionListener {
 	public static JLabel label;
 	public static Window window;
 	public static JTextArea text;
-	
+	public static TrayIcon trayIcon;
+	public static SystemTray tray;
+
 	public Window() {
 		panel = new JPanel();
 		button = new JButton("UpImg");
 		button.addActionListener(this);
 		label = new JLabel();
-		
+
 		button.setToolTipText("Click to upload an image");
 		panel.add(button);
 		panel.add(label);
 		this.add(panel);
+
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			System.out.println("Unable to set LookAndFeel");
+		}
+		if (SystemTray.isSupported()) {
+			tray = SystemTray.getSystemTray();
+
+			Image image = createImage("/images/favicon.png", "tray icon");
+			ActionListener exitListener = new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					System.exit(0);
+				}
+			};
+			PopupMenu popup = new PopupMenu();
+			MenuItem defaultItem = new MenuItem("Exit");
+			defaultItem.addActionListener(exitListener);
+			popup.add(defaultItem);
+			defaultItem = new MenuItem("Open");
+			defaultItem.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					setVisible(true);
+					setExtendedState(JFrame.NORMAL);
+				}
+			});
+			popup.add(defaultItem);
+			trayIcon = new TrayIcon(image, "SystemTray Demo", popup);
+			trayIcon.setImageAutoSize(true);
+		} else {
+			System.out.println("system tray not supported");
+		}
+		addWindowStateListener(new WindowStateListener() {
+			public void windowStateChanged(WindowEvent e) {
+				if (e.getNewState() == ICONIFIED) {
+					try {
+						tray.add(trayIcon);
+						setVisible(false);
+					} catch (AWTException ex) {
+						System.out.println("unable to add to tray");
+					}
+				}
+				if (e.getNewState() == 7) {
+					try {
+						tray.add(trayIcon);
+						setVisible(false);
+					} catch (AWTException ex) {
+						System.out.println("unable to add to system tray");
+					}
+				}
+				if (e.getNewState() == MAXIMIZED_BOTH) {
+					tray.remove(trayIcon);
+					setVisible(true);
+				}
+				if (e.getNewState() == NORMAL) {
+					tray.remove(trayIcon);
+					setVisible(true);
+				}
+			}
+		});
+		
 	}
-	
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == button) {
-			window.setState(ICONIFIED);
-			Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+			Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit()
+					.getScreenSize());
 			BufferedImage capture = null;
 			try {
 				capture = new Robot().createScreenCapture(screenRect);
@@ -58,5 +131,15 @@ public class Window extends JFrame implements ActionListener {
 			}
 		}
 	}
+	
+	protected static Image createImage(String path, String description) {
+        URL imageURL = Main.class.getResource(path);
+         
+        if (imageURL == null) {
+            System.err.println("Resource not found: " + path);
+            return null;
+        } else {
+            return (new ImageIcon(imageURL, description)).getImage();
+        }
+    }
 }
-
